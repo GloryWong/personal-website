f1288b83fdafe9ebcb0a8bb22d1f8afefd981922
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-05-28 20:57:07 +0800

    chore: version 0.0.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <header>
+      <h1>Zhao Zhao today</h1>
+    </header>
+    <main>
+      <div class="social">
+        <ul>
+          <li v-for="{ id, label, url } in social" :key="id">
+            <a :href="url" rel="noopener" target="_blank">
+              {{ label }}
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="project">
+        <ul>
+          <li v-for="{ id, label, url } in project" :key="id">
+            <a :href="url" rel="noopener" target="_blank">
+              {{ label }}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </main>
+    <footer>
+
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Vue } from "vue-class-component";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    social = [{
+    id: 'wechat',
+    label: 'Wechat',
+    action: () => {
+        // wcqcVisible = true;
+      }
+    }, {
+      id: 'weibo',
+      label: 'Weibo',
+      url: 'https://weibo.com/1768830500/profile?topnav=1&wvr=6&is_all=1'
+    }, {
+      id: 'neteasemusic',
+      label: 'Netease Music',
+      url: 'https://music.163.com/#/user/home?id=261618415'
+    }, {
+      id: 'codepen',
+      label: 'Code Pen',
+      url: 'https://codepen.io/glorywong'
+    }, {
+      id: 'facebook',
+      label: 'Facebook',
+      url: 'https://www.facebook.com/glorywongzhaohui'
+    }, {
+      id: 'twitter',
+      label: 'Twitter',
+      url: 'https://twitter.com/glorywong1001'
+    }, {
+      id: 'instagram',
+      label: 'Instagram',
+      url: 'https://www.instagram.com/glorywong1001/'
+    }, {
+      id: 'github',
+      label: 'Github',
+      url: 'https://github.com/glorywong'
+    }, {
+      id: 'email',
+      label: 'Email',
+      url: 'mailto:glorywong1001@gmail.com'
+    }];
+    
+    project = [{
+      id: 'covid19-xiaogan',
+      label: '孝感新冠疫情',
+      url: '/2019ncov-xiaogan/',
+      image: 'https://i.ibb.co/F4xxhSL/xiaogan-logo-64-64.png'
+    }];
+}
 </script>
