59acabedc26bc28252e09edd2c59b37b437c66e0
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-05-30 19:07:01 +0800

    chore: add dist

---
+++ dist/precache-manifest.8bb2dd1f69ecbc3c5cefc4b450772d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5068853a993a5e3b40d7f38248124191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a944fa3baa1b730720d",
+    "url": "/js/about.ab62c077.js"
+  },
+  {
+    "revision": "7a06978aec5fcc6330e4",
+    "url": "/js/app.a1eff28b.js"
+  },
+  {
+    "revision": "15b39404d6a2878c55f4",
+    "url": "/js/chunk-vendors.87a0165b.js"
+  },
+  {
+    "revision": "12f4ce736e44cdebe00b46acd355870f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
