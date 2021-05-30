016dd02f0b6b21a18935f28d908558dd043f403d
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-05-30 23:40:54 +0800

    chore: add config file

---
+++ src/config/index.ts
@@ -0,0 +1,61 @@
+const social = [
+  {
+    id: "wechat",
+    label: "Wechat",
+    action: (): void => {
+      // wcqcVisible = true;
+      return void 0;
+    },
+  },
+  {
+    id: "weibo",
+    label: "Weibo",
+    url: "https://weibo.com/1768830500/profile?topnav=1&wvr=6&is_all=1",
+  },
+  {
+    id: "neteasemusic",
+    label: "Netease Music",
+    url: "https://music.163.com/#/user/home?id=261618415",
+  },
+  {
+    id: "codepen",
+    label: "Code Pen",
+    url: "https://codepen.io/glorywong",
+  },
+  {
+    id: "facebook",
+    label: "Facebook",
+    url: "https://www.facebook.com/glorywongzhaohui",
+  },
+  {
+    id: "twitter",
+    label: "Twitter",
+    url: "https://twitter.com/glorywong1001",
+  },
+  {
+    id: "instagram",
+    label: "Instagram",
+    url: "https://www.instagram.com/glorywong1001/",
+  },
+  {
+    id: "github",
+    label: "Github",
+    url: "https://github.com/glorywong",
+  },
+  {
+    id: "email",
+    label: "Email",
+    url: "mailto:glorywong1001@gmail.com",
+  },
+];
+
+const project = [
+  {
+    id: "covid19-xiaogan",
+    label: "孝感新冠疫情",
+    url: "/2019ncov-xiaogan/",
+    image: "https://i.ibb.co/F4xxhSL/xiaogan-logo-64-64.png",
+  },
+];
+
+export { social, project };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,53 +31,10 @@
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
+import * as config from "@/config";
 
 export default class Home extends Vue {
-    social = [{
-    id: 'wechat',
-    label: 'Wechat',
-    action: () => {
-        // wcqcVisible = true;
-      }
-    }, {
-      id: 'weibo',
-      label: 'Weibo',
-      url: 'https://weibo.com/1768830500/profile?topnav=1&wvr=6&is_all=1'
-    }, {
-      id: 'neteasemusic',
-      label: 'Netease Music',
-      url: 'https://music.163.com/#/user/home?id=261618415'
-    }, {
-      id: 'codepen',
-      label: 'Code Pen',
-      url: 'https://codepen.io/glorywong'
-    }, {
-      id: 'facebook',
-      label: 'Facebook',
-      url: 'https://www.facebook.com/glorywongzhaohui'
-    }, {
-      id: 'twitter',
-      label: 'Twitter',
-      url: 'https://twitter.com/glorywong1001'
-    }, {
-      id: 'instagram',
-      label: 'Instagram',
-      url: 'https://www.instagram.com/glorywong1001/'
-    }, {
-      id: 'github',
-      label: 'Github',
-      url: 'https://github.com/glorywong'
-    }, {
-      id: 'email',
-      label: 'Email',
-      url: 'mailto:glorywong1001@gmail.com'
-    }];
-    
-    project = [{
-      id: 'covid19-xiaogan',
-      label: '孝感新冠疫情',
-      url: '/2019ncov-xiaogan/',
-      image: 'https://i.ibb.co/F4xxhSL/xiaogan-logo-64-64.png'
-    }];
+    social = config.social;
+    project = config.project;
 }
 </script>
