d28c5e117374022358cdb9950a3d9a4668cf9e64
GloryWong <glorywong1001@gmail.com>
2024-10-20 02:47:46 +0800

    pref: add the compression integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,15 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import playformCompress from '@playform/compress'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://glorywong.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    playformCompress(),
+  ],
 })
