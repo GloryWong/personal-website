1498c5d9f29c478dfd8a46e181dcb52cb3e219ed
GloryWong <glorywong1001@gmail.com>
2024-10-22 07:27:38 +0800

    fix: support external links for md

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import playformCompress from '@playform/compress'
 import { defineConfig } from 'astro/config'
+import rehypeExternalLinks from 'rehype-external-links'
 
 export default defineConfig({
   site: 'https://glorywong.com',
@@ -17,6 +18,15 @@ export default defineConfig({
       },
       wrap: true,
     },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['nofollow', 'noopener', 'noreferrer'],
+        },
+      ],
+    ],
   },
   integrations: [
     mdx(),
